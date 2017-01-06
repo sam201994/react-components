a3c24f261dd6e27d5f3837563a4db8676f1bad21
Hack Reactor Students <accounts@hackreactor.com>
2017-01-06 15:05:37 -0800

    Finish react components

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-// TODO
+var GroceryList = props => {
+  var css = `
+   .groceryItems:hover {
+      font-weight: bold;
+   }
+  `;
+  return (
+    <ul>
+      <style>
+        {css}
+      </style>
+      {
+        props.grocery.map((item, key) => (
+          <GroceryListItem key={key} grocery={item} />
+        ))
+      }
+    </ul>
+  );
+};
+
+// var GroceryListItem = props => (
+//   <li>{props.grocery}</li>
+// );
+
+var App = () => (
+  <div>
+    <GroceryList grocery={['carrot', 'radish']}/>
+  </div>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  onMo
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li key={this.props.key} className='groceryItems' style={style} onClick={this.onListItemClick.bind(this)} >{this.props.grocery}</li>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
